36802661c70ae7b5606f024e9825f40e31c3e60a
Elena-1717 <kea5599@mail.ru>
2024-07-08 19:55:15 +0300

    Update module_2_2.py

--- module_2_2.py
+++ module_2_2.py
@@ -3,8 +3,8 @@ second = int(input('Введите второе число: '))
 third = int(input('Введите третье число: '))
 
 if first == second == third:
-    print('Вывод: 3')
+    print(3)
 elif first == second or first == third or second == third:
-    print('Вывод: 2')
+    print(2)
 else:
-    print('Вывод: 0')
+    print(0)
